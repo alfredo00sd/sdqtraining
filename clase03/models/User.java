6ae208b13ad0803e79451ddb89af7b7a51fa3b16
Alfredo Acosta <alfredoz28mm@gmail.com>
2020-02-28 18:35:47 -0400

    Update User.java

--- clase03/models/User.java
+++ clase03/models/User.java
@@ -2,36 +2,36 @@ package reto1.clase03.models;
 
 public class User {
 
-    private static int userCounter;
-    private String name;
-    private String lastName;
-    private byte age;
+   	 private static int userCounter;
+	    private String name;
+	    private String lastName;
+	    private byte age;
 
-    public User(){
-        userCounter++;
-    }
+	    public User(){
+	        userCounter++;
+	    }
 
-    String getName() {
-        return name;
-    }
+	    public String getName() {
+	        return name;
+	    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	    public void setName(String name) {
+	        this.name = name;
+	    }
 
-    String getLastName() {
-        return lastName;
-    }
+	    public String getLastName() {
+	        return lastName;
+	    }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
 
-    public byte getAge() {
-        return age;
-    }
+	    public byte getAge() {
+	        return age;
+	    }
 
-    public void setAge(byte age) {
-        this.age = age;
-    }
+	    public void setAge(byte age) {
+	        this.age = age;
+	    }
 }
